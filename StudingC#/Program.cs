a54600f506f91232b72be18e99baf8e2e80c7679
YudiKojima <marcos.silva11@sou.ufmt.br>
2023-12-04 10:49:17 -0400

    aula 24,25,26

---
+++ StudingC#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+
+    //public static int ToDivide(int dividend, int divider, out int rest)
+    //{
+    //    int result = dividend / divider;
+    //    rest = dividend % divider;
+    //    return result;
+    //}
+
+    //public static void ToDouble(ref int num)
+    //{
+    //    num *= 2;
+    //}
+
+    //static decimal GetTestAverage()
+    //{
+    //    decimal p1, p2, averageDecimal;
+
+    //    Console.WriteLine("Write your test notes: ");
+
+    //    p1 = Convert.ToDecimal(Console.ReadLine());
+    //    p2 = Convert.ToDecimal(Console.ReadLine());
+    //    averageDecimal = GetTestAverage(p1, p2);
+    //    Console.WriteLine("Your average in tests is: " + averageDecimal);
+
+    //    return (p1 + p2) / 2;
+    //}
+
+}
